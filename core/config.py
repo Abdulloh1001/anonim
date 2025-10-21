5b473463c4bdab4029c39730c8ed5e41066fb224
abdulloh1001 <leoncrow070@gmail.com>
2025-10-22 00:10:09 +0500

    Fix: Default CHANNEL_ID qo'shildi

--- core/config.py
+++ core/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "eclipse_1oo1")  # Kanal usernamesi @ belgisisiz
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # Majburiy obuna uchun kanal ID
+CHANNEL_ID = os.getenv("CHANNEL_ID", "-100123456789")  # O'z kanal ID raqamingizni kiriting
 SECRET_KEY = os.getenv("SECRET_KEY", "anon123")
 REF_REWARD = 5
 PHOTO_TOKEN_THRESHOLD = 50
